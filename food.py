00ae4376276850648406ad2f2a2ad40f303c32b0
Carson Bennett <bennec4222@clarkstate.edu>
2026-03-01 17:32:03 -0500

    csd-1510-programming-fundementals-master-lists-CarsonBennett

--- food.py
+++ food.py
@@ -14,3 +14,18 @@
 #Your entered foods are:
 #pizza, beef jerkey, rice triangles, steamed chinese bun, fried chicken 
 #You entered a total of 62 characters
+
+foods = []
+
+for i in range(5):
+    food = input("Enter a food: ")
+    foods.append(food)
+
+print("\nYour entered foods are:")
+ print(", ".join(foods))
+
+total_characters = 0 
+for item in foods:
+    total_characters += len(item)
+
+print(f"You entered a total of {total_characters} characters.")
--- meanmedian.py
+++ meanmedian.py
@@ -4,3 +4,29 @@
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
 # video url: https://youtu.be/VGrRdUunjXg
+
+theList = []
+
+for i in range(5):
+    thelist.append(int(input(f"Number {i+1}:")))
+
+print("as entered: " + str(theList))
+
+theList.sort()
+
+print(f"small to Large: {theList}")
+
+theList.sort(reverse=True)
+
+print(f"large to small: {theList}")
+
+median = the List[2]
+
+sum = 0
+
+for i in range(5):
+    sum += theList[i] 
+
+mean = sum / 5
+
+print(f"The median is {m,edian} and the mean is {mean}")
